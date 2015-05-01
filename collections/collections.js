2e53b115993fc979b707a2846be865c0111b3a87
Connor <cromwellslakey@gmail.com>
2015-05-01 07:19:29 -0700

    "dom ex"

--- collections/collections.js
+++ collections/collections.js
@@ -1,18 +1,22 @@
 //
 // Part 1
 //
-
-
-
-
+var liList = document.getElementsByTagName('li');
+for(var i = 0; i < liList.length; i++){
+    liList[i].style.borderBottom = 'thin dotted red';
+};
 //
 // Part 2
 //
-
-
-
+var redMaker = document.getElementsByClassName('datatype');
+for(var i = 0; i < redMaker.length; i++){
+    redMaker[i].style.color = 'red';
+};
 
 //
 // Part 3
 //
-
+var blueMaker = document.querySelectorAll('li.function');
+for(var i = 0; i < blueMaker.length; i++){
+    blueMaker[i].style.color = 'blue';
+};
--- events/events.js
+++ events/events.js
@@ -1,1 +1,24 @@
 // Your code here
+//prob 1
+	var alertC = document.getElementById('clicker');
+	alertC.addEventListener('click' , function (event){
+    alert('clicked');
+});
+//prob 2
+	var bg = document.getElementById('mouser');
+	bg.addEventListener('mouseover' , function (event){
+    bg.style.backgroundColor = 'red'	
+});
+//prob 3
+	var bg = document.getElementById('mouser');
+	bg.addEventListener('mouseout' , function (event){
+    bg.style.backgroundColor = 'red'	
+});
+
+//prob 4 
+var submitInput = document.querySelector('input')
+var submitter = document.getElementById('former');
+	submitter.addEventListener('submit' , function (event){
+    event.preventDefault();
+    alert(submitInput.value);
+});
